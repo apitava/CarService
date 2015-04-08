46ed510bf508486c2218c2ab5a9670a577f61063
apitava <apitava@live.com>
2015-04-08 18:28:56 -0400

    App Start

---
+++ CarService/Main/NewOrderForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class NewOrderForm : Form
+    {
+        public Order NewOrderObject = null;
+
+        public NewOrderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            NewOrderObject.ClientName = "zura";
+            N
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+    }
+}
